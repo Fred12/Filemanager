eebd70029aafcc882dd75d7b71e829562ee04b1f
Stefan <Stefan@Speedy3>
2013-08-11 16:54:22 +0200

    - ActionListener zu jedem Menüpunkt hinzugefügt. - Funktionsrumpf für jeden Menüpunkt bereitgestellt (in der erstellen ActionListener Klasse) - Einbindung von Aktionen für alle Menüpunkte nun direkt möglich

--- src/com/kandinsky/gui/KandinskyMenuBar.java
+++ src/com/kandinsky/gui/KandinskyMenuBar.java
@@ -30,21 +30,25 @@ public class KandinskyMenuBar extends JMenuBar {
 		
 		JMenuItem menuItemSwapFolderViewLeftRight = new JMenuItem("Ordner rechts/links tauschen");
 		menuFile.add(menuItemSwapFolderViewLeftRight);
+		menuItemSwapFolderViewLeftRight.addActionListener(functions);		
 		
 		JSeparator separator_5 = new JSeparator();
 		menuFile.add(separator_5);
 		
 		JMenuItem menuItemImportFavorites = new JMenuItem("Favoriten importieren");
 		menuFile.add(menuItemImportFavorites);
+		menuItemImportFavorites.addActionListener(functions);
 		
 		JMenuItem menuItemExportFavorites = new JMenuItem("Favoriten exportieren");
 		menuFile.add(menuItemExportFavorites);
+		menuItemExportFavorites.addActionListener(functions);
 		
 		JSeparator separator = new JSeparator();
 		menuFile.add(separator);
 		
 		JMenuItem menuItemCloseProgram = new JMenuItem("Schliessen");
 		menuFile.add(menuItemCloseProgram);
+		menuItemCloseProgram.addActionListener(functions);
 		
 		// Edit menu section
 		
@@ -53,39 +57,49 @@ public class KandinskyMenuBar extends JMenuBar {
 		
 		JMenuItem menuItemSelectAll = new JMenuItem("Alles ausw\u00E4hlen");
 		menuEdit.add(menuItemSelectAll);
+		menuItemSelectAll.addActionListener(functions);
 		
 		JMenuItem menuItemSelectNothing = new JMenuItem("Nichts ausw\u00E4hlen");
 		menuEdit.add(menuItemSelectNothing);
+		menuItemSelectNothing.addActionListener(functions);
 		
 		JMenuItem menuItemInvertSelection = new JMenuItem("Auswahl umkehren");
 		menuEdit.add(menuItemInvertSelection);
+		menuItemInvertSelection.addActionListener(functions);
 		
 		JSeparator separator_6 = new JSeparator();
 		menuEdit.add(separator_6);
 		
 		JMenuItem menuItemCopy = new JMenuItem("Kopieren");
 		menuEdit.add(menuItemCopy);
+		menuItemCopy.addActionListener(functions);
 		
 		JMenuItem menuItemPaste = new JMenuItem("Einf\u00FCgen");
 		menuEdit.add(menuItemPaste);
+		menuItemPaste.addActionListener(functions);
 		
 		JMenuItem menuItemCut = new JMenuItem("Ausschneiden");
 		menuEdit.add(menuItemCut);
+		menuItemCut.addActionListener(functions);
 		
 		JMenuItem menuItemDelete = new JMenuItem("L\u00F6schen");
 		menuEdit.add(menuItemDelete);
+		menuItemDelete.addActionListener(functions);
 		
 		JSeparator separator_1 = new JSeparator();
 		menuEdit.add(separator_1);
 		
 		JMenuItem menuItemCreateFolder = new JMenuItem("Ordner anlegen");
 		menuEdit.add(menuItemCreateFolder);
+		menuItemCreateFolder.addActionListener(functions);
 		
 		JMenuItem menuItemCreateShortcut = new JMenuItem("Verkn\u00FCpfung anlegen");
 		menuEdit.add(menuItemCreateShortcut);
+		menuItemCreateShortcut.addActionListener(functions);
 		
 		JMenuItem menuItemCreateEmptyFile = new JMenuItem("Leere Datei anlegen");
 		menuEdit.add(menuItemCreateEmptyFile);
+		menuItemCreateEmptyFile.addActionListener(functions);
 		
 		// FTP Menu Section
 		
@@ -94,24 +108,21 @@ public class KandinskyMenuBar extends JMenuBar {
 		
 		JMenuItem menuItemEditServerList = new JMenuItem("Server verwalten");
 		menuFTP.add(menuItemEditServerList);
+		menuItemEditServerList.addActionListener(functions);
 		
 		JSeparator separator_2 = new JSeparator();
 		menuFTP.add(separator_2);
 		
 		JMenu menuFTPConnect = new JMenu("Verbinden");
 		menuFTP.add(menuFTPConnect);
+		menuFTPConnect.addActionListener(functions);
 		
 		JMenuItem menuItemFTPServerEntryDummy = new JMenuItem("Serverliste hier erzeugen");
 		menuFTPConnect.add(menuItemFTPServerEntryDummy);
-		
+				
 		JMenuItem menuItemFTPCloseConnection = new JMenuItem("Verbindung trennen");
 		menuFTP.add(menuItemFTPCloseConnection);
-		
-		JSeparator separator_3 = new JSeparator();
-		menuFTP.add(separator_3);
-		
-		JCheckBoxMenuItem menuCheckboxToggleShowHiddenFiles = new JCheckBoxMenuItem("Versteckte Dateien anzeigen");
-		menuFTP.add(menuCheckboxToggleShowHiddenFiles);
+		menuItemFTPCloseConnection.addActionListener(functions);
 		
 		// Settings menu section
 		
@@ -120,18 +131,26 @@ public class KandinskyMenuBar extends JMenuBar {
 		
 		JMenuItem menuItemGeneralOptions = new JMenuItem("Allgemeine Optionen");
 		menuSettings.add(menuItemGeneralOptions);
+		menuItemGeneralOptions.addActionListener(functions);
 		
 		JMenuItem menuItemEditFavorites = new JMenuItem("Favoriten verwalten");
 		menuSettings.add(menuItemEditFavorites);
+		menuItemEditFavorites.addActionListener(functions);
 		
 		JMenuItem menuItemFTPOptions = new JMenuItem("FTP Optionen");
 		menuSettings.add(menuItemFTPOptions);
+		menuItemFTPOptions.addActionListener(functions);
 		
 		JSeparator separator_4 = new JSeparator();
 		menuSettings.add(separator_4);
 		
+		JCheckBoxMenuItem menuCheckboxToggleShowHiddenFiles = new JCheckBoxMenuItem("Versteckte Dateien anzeigen");
+		menuSettings.add(menuCheckboxToggleShowHiddenFiles);
+		menuCheckboxToggleShowHiddenFiles.addActionListener(functions);
+		
 		JCheckBoxMenuItem menuCheckboxToggleShowFavorites = new JCheckBoxMenuItem("Favoritenliste anzeigen");
 		menuSettings.add(menuCheckboxToggleShowFavorites);
+		menuCheckboxToggleShowFavorites.addActionListener(functions);
 		
 		// Help Menu section
 		
@@ -140,9 +159,11 @@ public class KandinskyMenuBar extends JMenuBar {
 		
 		JMenuItem menuItemHelp = new JMenuItem("Hilfe");
 		menuHelp.add(menuItemHelp);
+		menuItemHelp.addActionListener(functions);
 		
 		JMenuItem menuItemAbout = new JMenuItem("\u00DCber Kandinsky File Manager");
 		menuHelp.add(menuItemAbout);
+		menuItemAbout.addActionListener(functions);
 		
 	}
 	
--- src/com/kandinsky/gui/KandinskyMenuBarFunctions.java
+++ src/com/kandinsky/gui/KandinskyMenuBarFunctions.java
@@ -6,19 +6,165 @@ import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
 public class KandinskyMenuBarFunctions implements ActionListener {
-
+	
+	// Menu "File" Items
+	public static final String RELOAD 					= "Aktualisieren";
+	public static final String SEARCH 					= "Suchen";
+	public static final String SWAP_FOLDERS_LEFT_RIGHT	= "Ordner rechts/links tauschen";
+	public static final String IMPORT_FAVORITES			= "Favoriten importieren";
+	public static final String EXPORT_FAVORITES			= "Favoriten exportieren";
+	public static final String CLOSE_PROGRAM			= "Schliessen";
+	
+	// Menu "Edit" Items
+	public static final String SELECT_ALL				= "Alles ausw\u00E4hlen";
+	public static final String SELECT_NOTHING			= "Nichts ausw\u00E4hlen";
+	public static final String INVERT_SELECTION			= "Auswahl umkehren";
+	public static final String COPY						= "Kopieren";
+	public static final String PASTE					= "Einf\u00FCgen";
+	public static final String CUT						= "Ausschneiden";
+	public static final String DELETE					= "L\u00F6schen";
+	public static final String CREATE_FOLDER			= "Ordner anlegen";
+	public static final String CREATE_SHORTCUT			= "Verkn\u00FCpfung anlegen";
+	public static final String CREATE_EMPTY_FILE		= "Leere Datei anlegen";
+	
+	// Menu "FTP" Items
+	public static final String FTP_EDIT_SERVERS			= "Server verwalten";
+	public static final String FTP_CONNECT				= "Verbinden";
+	public static final String FTP_DISCONNECT			= "Verbindung trennen";
+	
+	
+	// Menu "Settings" Items
+	public static final String OPTIONS					= "Allgemeine Optionen";
+	public static final String OPTIONS_FAVORITES		= "Favoriten verwalten";
+	public static final String OPTIONS_FTP				= "FTP Optionen";
+	public static final String TOGGLE_HIDDEN_FOLDERS	= "Versteckte Dateien anzeigen";
+	public static final String TOGGLE_SHOW_FAVORITES	= "Favoritenliste anzeigen";
+		
+	// Menu "Help" Items
+	public static final String HELP						= "Hilfe";
+	public static final String ABOUT					= "Über Kandinsky File Manager";
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		String source = e.getActionCommand();
 		
-		if(source.equals("Aktualisieren")) {
+		switch(source) {
+		
+		// Menu "File" Actions
+		
+		case RELOAD:
 			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
-		}
-		else if(source.equals("Suchen")) {
+			break;
+		
+		case SEARCH:
 			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
-		}
+			break;
+		
+		case SWAP_FOLDERS_LEFT_RIGHT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
 
+		case IMPORT_FAVORITES:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case EXPORT_FAVORITES:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case CLOSE_PROGRAM:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		
+		// Menu "Edit" Actions
+		case SELECT_ALL:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case SELECT_NOTHING:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case INVERT_SELECTION:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case COPY:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		
+		case PASTE:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case CUT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		
+		case DELETE:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case CREATE_FOLDER:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case CREATE_SHORTCUT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		
+		case CREATE_EMPTY_FILE:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		// Menu "FTP" Actions
+		case FTP_EDIT_SERVERS:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case FTP_CONNECT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case FTP_DISCONNECT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		// Menu "Options" Actions
+		case OPTIONS:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case OPTIONS_FAVORITES:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case OPTIONS_FTP:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case TOGGLE_SHOW_FAVORITES:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		
+		case TOGGLE_HIDDEN_FOLDERS:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		// Menu "Help" Actions
+		case HELP:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+			
+		case ABOUT:
+			JOptionPane.showMessageDialog(null, "Selected Item: " + e.getActionCommand());
+			break;
+		}
+		
+		
+		
+	
 	}
 
 }
