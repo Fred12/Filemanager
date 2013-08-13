c6f7a07125216bc0616f731fd2fefdc12c2a5ec4
schmidtb <benedikt.schmidt@gmx.de>
2013-08-13 11:08:07 +0200

    ButtonBar hinzugefügt

---
+++ src/com/kandinsky/gui/ButtonBar.java
@@ -0,0 +1,13 @@
+package com.kandinsky.gui;
+
+import javax.swing.JPanel;
+
+/**
+ * PUNKT 3 - ButtonBar.
+ * Zeigt eine Liste mit Buttons. Hier ist zu überlegen, ob die Buttons für beide Seiten angezeigt werden sollen,
+ * oder nur für eine Seite.
+ * @author schmidtb
+ */
+public class ButtonBar extends JPanel {
+
+}
